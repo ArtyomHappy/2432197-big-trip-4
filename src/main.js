c48170a0c0ae8ad5d2ea87798a4ddf3db96f7425
ArtyomHappy <bunkov.art@list.ru>
2024-04-14 21:45:31 +0500

    Шаблонизируй это

--- src/main.js
+++ src/main.js
@@ -1,16 +1,41 @@
 import { render, RenderPosition } from './render';
 import Destination from './view/destination';
 import Filter from './view/filter';
-import Sorting from './view/sorting';
 import RoutePresenter from './presenter/route-presenter';
+import MockService from './service/mock-service';
+import DestinationsModel from './model/destination-model';
+import OffersModel from './model/offers-model';
+import PointsModel from './model/points-model';
+
+// const siteDestinationContainer = document.querySelector('.trip-main');
+// const siteFilterContainer = document.querySelector('.trip-controls__filters');
+// const siteSortContainer = document.querySelector('.trip-events');
+// const routePresenter = new RoutePresenter({routeContainer: siteSortContainer});
+
+// render(new Destination(), siteDestinationContainer, RenderPosition.AFTERBEGIN);
+// render(new Filter(), siteFilterContainer);
+// render(new Sorting(), siteSortContainer);
+
+// routePresenter.init();
 
 const siteDestinationContainer = document.querySelector('.trip-main');
 const siteFilterContainer = document.querySelector('.trip-controls__filters');
 const siteSortContainer = document.querySelector('.trip-events');
-const routePresenter = new RoutePresenter({routeContainer: siteSortContainer});
+
+const mockService = new MockService();
+
+const destinationsModel = new DestinationsModel(mockService);
+const offersModel = new OffersModel(mockService);
+const pointsModel = new PointsModel(mockService);
+
+const routePresenter = new RoutePresenter({
+  tripContainer: siteSortContainer,
+  destinationsModel,
+  offersModel,
+  pointsModel
+});
 
 render(new Destination(), siteDestinationContainer, RenderPosition.AFTERBEGIN);
 render(new Filter(), siteFilterContainer);
-render(new Sorting(), siteSortContainer);
 
 routePresenter.init();
--- src/presenter/route-presenter.js
+++ src/presenter/route-presenter.js
@@ -1,23 +1,38 @@
 import RouteInfo from '../view/route-info';
-import NewEvent from '../view/new-event';
 import EditorEvent from '../view/editor-event';
 import RoutePoint from '../view/route-point';
+import Sorting from '../view/sorting';
 import { render } from '../render';
 
-export default class RoutePresenter {
-  tripEventsComponent = new RouteInfo();
-
-  constructor({routeContainer}) {
-    this.routeContainer = routeContainer;
+export default class TripPresenter {
+  constructor( {tripContainer, destinationsModel, offersModel, pointsModel} ) {
+    this.tripContainer = tripContainer;
+    this.pointList = new RouteInfo();
+    this.destinationsModel = destinationsModel;
+    this.offersModel = offersModel;
+    this.pointsModel = pointsModel;
+    this.points = [...this.pointsModel.get()];
   }
 
   init() {
-    render(this.tripEventsComponent, this.routeContainer);
-    render(new EditorEvent(), this.tripEventsComponent.getElement());
-    render(new NewEvent(), this.tripEventsComponent.getElement());
+    render(new Sorting(), this.tripContainer);
+    render(this.pointList, this.tripContainer);
+    render(
+      new EditorEvent({
+        point: this.points[0],
+        pointDestination: this.destinationsModel.getById(this.points[0].destination),
+        pointOffers: this.offersModel.getByType(this.points[0].type),
+      }), this.pointList.getElement());
 
-    for (let i = 0; i < 3; i++) {
-      render(new RoutePoint(), this.tripEventsComponent.getElement());
-    }
+    this.points.forEach((point) => {
+      render(
+        new RoutePoint({
+          point,
+          pointDestination: this.destinationsModel.getById(point.destination),
+          pointOffers: this.offersModel.getByType(point.type)
+        }),
+        this.pointList.getElement()
+      );
+    });
   }
 }
